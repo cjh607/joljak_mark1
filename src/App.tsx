4af739c1ff46faebe640a2bf3e7492960a663a74
Your Name <cjh_607@naver.com>
2025-04-01 00:21:37 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [depart, setDepart] = useState([]);
+    let name : string[] = ["hhihi","nynyyn"];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>졸작 화이팅</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        axios
+            .get('http://localhost:8080/api')
+            .then((response) => {
+                console.log(response.data);
+                setDepart(response.data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Departments</h1>
+            <ul>
+                {depart.department.map(dept => (
+                    <li key={dept.id}>{dept.id} {dept.departmentName}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
